47f772021341d9454be8374b9fbb4c436efc9559
Mark Goericke <mark@javamark.de>
2018-06-22 12:45:34 +0200

    basic sorting without type definitions

--- java8-sorting/src/test/java/de/javamark/java8/model/BasicSortWithLambdaTest.java
+++ java8-sorting/src/test/java/de/javamark/java8/model/BasicSortWithLambdaTest.java
@@ -8,7 +8,10 @@ import java.util.List;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+
+
 public class BasicSortWithLambdaTest {
+
     @Test
     public void whenSortByName_ResultShouldBeSortedCorrectly() {
 
@@ -22,6 +25,20 @@ public class BasicSortWithLambdaTest {
                 (Student s1, Student s2) -> s1.getName().compareTo(s2.getName()));
 
         assertThat(students.get(0).getName(), equalTo("Emma"));
+    }
 
+    @Test
+    public void givenLambdaShortForm_whenSortByName_ResultShouldBeSortedCorrectly(){
+
+        List<Student> students = Arrays.asList(
+                new Student("Max", 23),
+                new Student("Emma", 21),
+                new Student("Klaus", 20)
+        );
+
+        // same as above but without type definitions
+        students.sort((s1, s2) -> s1.getName().compareTo(s2.getName()));
+
+        assertThat(students.get(0).getName(), equalTo("Emma"));
     }
 }
